90056cdc42d3fd8e00c8a2bf2777c2c00ff150f4
Jaeguki <dev.jaeguki@gmail.com>
2019-05-02 17:37:14 +0900

    os classification code update

--- src/main/java/com/cafe24/network/chat/server/ChatServer.java
+++ src/main/java/com/cafe24/network/chat/server/ChatServer.java
@@ -8,7 +8,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Base64;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
 
 public class ChatServer {
 	public static final int PORT = 5000;
@@ -17,12 +21,52 @@ public class ChatServer {
 		ServerSocket serverSocket = null;
 		List<PrintWriter> listWriters = new ArrayList<PrintWriter>();
 		
+		String os = System.getProperty("os.name").toUpperCase();
+		String hostAddress = "";
+
+		if(os.contains("LINUX")) {
+			try {
+				boolean isLoopBack = true;
+				Enumeration<NetworkInterface> en;
+				en = NetworkInterface.getNetworkInterfaces();
+				
+				while(en.hasMoreElements()) {
+					NetworkInterface ni = en.nextElement();
+					if(ni.isLoopback())
+						continue;
+					
+					Enumeration<InetAddress> inetAddresses = ni.getInetAddresses();
+					while(inetAddresses.hasMoreElements()){
+						InetAddress ia = inetAddresses.nextElement();
+						if(ia.getHostAddress() != null && ia.getHostAddress().indexOf(".") != -1) {
+							hostAddress = ia.getHostAddress();
+							// System.out.println(hostAddress);
+							isLoopBack = false;
+							break;
+						}
+					}
+					if(isLoopBack)
+						break;
+				}
+				// System.out.println("IP = " + hostAddress);
+			}catch(SocketException e) {
+				e.printStackTrace();
+			}
+		}else {
+			try {
+				hostAddress = InetAddress.getLocalHost().getHostAddress();
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		
 		try {
 			// 1. 서버 소켓 생성
 			serverSocket = new ServerSocket();
 			
 			// 2. 바인딩
-			String hostAddress = InetAddress.getLocalHost().getHostAddress();
 			serverSocket.bind( new InetSocketAddress(hostAddress, PORT) );
 			consoleLog("Wait Connection " + hostAddress + ":" + PORT);
 			
